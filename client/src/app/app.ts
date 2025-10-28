5012a8091c68a2f50bee892911702e47640dc0e2
Raymond <yulirice@gmail.com>
2025-10-28 23:09:11 +0800

    End of section 3

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +13,17 @@ export class App implements OnInit {
   protected title = 'client';
   protected members = signal<any>([]);
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/members').subscribe({
-      next: response => this.members.set(response),
-      error: (error) => console.error(error),
-      complete: () => console.log('Completed the http request')
-    });
+  async ngOnInit() {
+    this.members.set( await this.getMembers());
+  }
+
+  async getMembers(){
+    try{
+       return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    }catch(error){
+      console.log(error);
+      throw error;
+    }
+    
   }
 }
